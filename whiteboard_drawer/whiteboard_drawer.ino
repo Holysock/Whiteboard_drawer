34aca9c8ab3f2cd4a2cc8172868fab5aefc79567
Jan--Henrik <janhenrik@janhenrik.org>
2017-10-25 15:29:58 +0200

    added Serial GCODE parser

--- whiteboard_drawer/whiteboard_drawer.ino
+++ whiteboard_drawer/whiteboard_drawer.ino
@@ -4,6 +4,7 @@
 
 #include <StepControl.h>
 #include <SoftwareSerial.h>   // We need this even if we're not using a SoftwareSerial object
+
 // Due to the way the Arduino IDE compiles
 
 #define HWSERIAL Serial1
@@ -17,17 +18,28 @@
 #define SPEED_MAX 250
 #define DIAMETER 38
 #define LENGTH_PER_REVOLUTION  (DIAMETER * PI) //mm
-#define LPS (LENGTH_PER_REVOLUTION / STEP_PER_REVOLUTION) 
+#define LPS (LENGTH_PER_REVOLUTION / STEP_PER_REVOLUTION)
 
-struct Position_in_M1_Struct{
+#define SERIALBUFFERLEN 40 //bufferlength of serial parser
+
+struct Position_in_M1_Struct {
   float x;
   float y;
 };
 
-struct Position_in_M2_Struct{
+struct Position_in_M2_Struct {
   float a;
   float b;
 };
+
+float xPos;  //x pos
+float yPos; //y pos
+float zPos; //z pos
+
+char *pos2; //pointer 1
+char *pos1; //pos pointer 2
+
+char Message[SERIALBUFFERLEN]; // incoming String
 
 struct Position_in_M1_Struct pos_in_M1;
 struct Position_in_M2_Struct pos_in_M2;
@@ -42,38 +54,67 @@ char serialData[8];
 void setup()
 {
 
-  Serial.begin(9600);
+  Serial.begin(115200);
   HWSERIAL.begin(115200);
 
-// setup the motors
+  // setup the motors
   motor_b
   .setMaxSpeed(SPEED_MAX)       // steps/s
   .setAcceleration(10000000); // steps/s^2
 
   motor_a
-  .setMaxSpeed(SPEED_MAX)       // steps/s 
+  .setMaxSpeed(SPEED_MAX)       // steps/s
   .setAcceleration(100000000); // steps/s^2
 
   delay(5000);
 
   /*pos_in_M1.x = 200;
-  pos_in_M1.y = 0;
-  Serial.printf("Original: x: %f y: %f \n", pos_in_M1.x, pos_in_M1.y);
+    pos_in_M1.y = 0;
+    Serial.printf("Original: x: %f y: %f \n", pos_in_M1.x, pos_in_M1.y);
 
-  pos_in_M1 = to_M1(to_M2(pos_in_M1));
-  Serial.printf("Back in M1 x: %f y: %f \n", pos_in_M1.x, pos_in_M1.y);
+    pos_in_M1 = to_M1(to_M2(pos_in_M1));
+    Serial.printf("Back in M1 x: %f y: %f \n", pos_in_M1.x, pos_in_M1.y);
+
+
+    pos_in_M1.x = (LENGTH_C / 2.0) - 300;
+    pos_in_M1.y = 0;
+    line(pos_in_M1);
   */
-
-  pos_in_M1.x = (LENGTH_C / 2.0) - 300;
-  pos_in_M1.y = 0;
-  line(pos_in_M1);
-
 
 }
 
 void loop()
 {
- 
+  if (Serial.available() > 0) { //skip unconnected slots
+    memset(&Message[0], 0, sizeof(Message)); //clear the array as the message length varies
+    pos1 = 0; //needs to be cleared for every client
+    pos2 = 0; //this one too
+    for (int j = 0; j < SERIALBUFFERLEN - 1; j++) { //fetch the message
+      Message[j] = Serial.read(); //faster the readBytesUntil()
+      if (Message[j] == '\n') { //exit on newline
+        break;
+      }
+    }
+
+    if (!strncmp(Message, "G1 ", 3) || !strncmp(Message, "G0 ", 3)) { //parsing part, it works...
+      for (int j = 3; j < SERIALBUFFERLEN - 1; j++) { //iterate throu the message
+        if (Message[j] == 'X') {  //check for an X
+          pos1 = Message + j; //set pointer
+          while (Message[++j] == ' ')pos1++;  //increment until next whitespace
+          xPos = strtof(Message + j, &pos1);  //get float value
+        } else if (Message[j] == 'Y') { //check for an Y
+          pos1 = Message + j; //set pointer
+          while (Message[++j] == ' ')pos1++; //increment until next whitespace
+          yPos = strtof(Message + j, &pos1); //get float value
+        } else if (Message[j] == 'Z') { //check for an Z
+          pos1 = Message + j; //set pointer
+          while (Message[++j] == ' ')pos1++; //increment until next whitespace
+          zPos = strtof(Message + j, &pos1); //get float value
+        }
+      }
+    }
+  }
+
   pos_in_M1.y = 600;
   line(pos_in_M1);
 
@@ -83,10 +124,10 @@ void loop()
   pos_in_M1.y = 0;
   line(pos_in_M1);
 
-  pos_in_M1.x = (LENGTH_C / 2.0) -300;
+  pos_in_M1.x = (LENGTH_C / 2.0) - 300;
   //pos_in_M1.y = 0;
   line(pos_in_M1);
-  
+
 }
 
 
@@ -100,8 +141,8 @@ void loop()
 
 
 // M1 = kartesian, M2 = custom triangle whiteboad system
-// Transformation M1 -> M2 
-struct Position_in_M2_Struct to_M2(Position_in_M1_Struct M1_pos){
+// Transformation M1 -> M2
+struct Position_in_M2_Struct to_M2(Position_in_M1_Struct M1_pos) {
   struct Position_in_M2_Struct M2_pos;
   M2_pos.a = sqrt( pow(M1_pos.x, 2) +  pow(M2_TRANSLATION_Y - M1_pos.y, 2));
   M2_pos.b = sqrt( pow(M1_pos.x - LENGTH_C, 2) +  pow(M2_TRANSLATION_Y - M1_pos.y, 2));
@@ -109,49 +150,49 @@ struct Position_in_M2_Struct to_M2(Position_in_M1_Struct M1_pos){
 }
 
 // Transformation M2 -> M1
-struct Position_in_M1_Struct to_M1(Position_in_M2_Struct M2_pos){
+struct Position_in_M1_Struct to_M1(Position_in_M2_Struct M2_pos) {
   struct Position_in_M1_Struct M1_pos;
   float a = M2_pos.a;
   float b = M2_pos.b;
   float c = LENGTH_C;
-  float cos_alpha = (c*c + b*b - a*a) / (2*b*c);
-  M1_pos.x = LENGTH_C - b*cos_alpha;
-  M1_pos.y = M2_TRANSLATION_Y - b*sin(acos(cos_alpha)); //use if thread length is limited
+  float cos_alpha = (c * c + b * b - a * a) / (2 * b * c);
+  M1_pos.x = LENGTH_C - b * cos_alpha;
+  M1_pos.y = M2_TRANSLATION_Y - b * sin(acos(cos_alpha)); //use if thread length is limited
   return M1_pos;
 }
 
-int length2step(float l){
+int length2step(float l) {
   return (LENGTH_MAX - l) / LPS;
 }
 
-float step2length(int s){
+float step2length(int s) {
   return (float)(LENGTH_MAX - s * LPS);
 }
 
-void line(Position_in_M1_Struct M1_target){
+void line(Position_in_M1_Struct M1_target) {
   struct Position_in_M2_Struct M2_cur;
   struct Position_in_M2_Struct M2_tmp;
   struct Position_in_M1_Struct M1_cur;
   struct Position_in_M1_Struct M1_del;
-  
+
   M2_cur.a = step2length(motor_a.getPosition());
   M2_cur.b = step2length(motor_b.getPosition());
 
   M1_cur = to_M1(M2_cur);
-  
+
   float x_d = M1_target.x - M1_cur.x;
   float y_d = M1_target.y - M1_cur.y;
-  float distance = sqrt(x_d*x_d + y_d*y_d);
+  float distance = sqrt(x_d * x_d + y_d * y_d);
   float divisor = (int)(distance / DELTA_L);
-  if(divisor < 1) divisor = 1;
-  
-  for(int i=1;i<=divisor;i++){
-      M1_del.x = M1_cur.x + x_d * (i/divisor);
-      M1_del.y = M1_cur.y + y_d * (i/divisor);
-      M2_tmp = to_M2(M1_del);
-      motor_a.setTargetAbs(length2step(M2_tmp.a));
-      motor_b.setTargetAbs(length2step(M2_tmp.b));
-      controller.move(motor_a, motor_b);    
+  if (divisor < 1) divisor = 1;
+
+  for (int i = 1; i <= divisor; i++) {
+    M1_del.x = M1_cur.x + x_d * (i / divisor);
+    M1_del.y = M1_cur.y + y_d * (i / divisor);
+    M2_tmp = to_M2(M1_del);
+    motor_a.setTargetAbs(length2step(M2_tmp.a));
+    motor_b.setTargetAbs(length2step(M2_tmp.b));
+    controller.move(motor_a, motor_b);
   }
 }
 
